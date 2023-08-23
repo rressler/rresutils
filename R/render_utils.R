98e5580665af4111952ea94dc5ee31d93c8cb1b3
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2023-08-23 17:17:06 -0400

    update for QUarto

--- R/render_utils.R
+++ R/render_utils.R
@@ -1,34 +1,37 @@
 #' Render two versions of a file, with and without solutions
 #'
-#' This function assumes the YAML header of a .qmd file uses a parameter called solutions, params: solutions which has two values, TRUE and FALSE. The file may use this parameter to adjust the r markdown chunk options such as eval or echo when rendering to output the solutions to exercises or not.
+#' This function assumes the YAML header of a .qmd file uses a parameter
+#' called solutions, params: solutions which has two values, TRUE and FALSE.
+#' The file may use this parameter to adjust the R markdown chunk options
+#' such as eval or echo when rendering to output the solutions to exercises.
 #'
-#' The function may be placed in the YAML header of a .qmd file where it will be invoked on the first output type.
+#' The function works as a standalone function called in the console or script.
 #'
-#' See Rmarkdown Cookbook Section 17.5 as a reference
-#' https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html
 #'
 #' @param input text: the name of the file to be rendered
 #' @param fext text for the file extension
 #' @param ... text: other parameters to be passed to the render function
 #'
-#' @return creates two output files in the default directory. both have the current date appended to the file name. One has no solutions in the output and the second has solutions and the word solutions in the output file name.
+#' @return creates two output files in the default directory. Both have the
+#' current date appended to the file name. One has no solutions in the output
+#' and the second has solutions and the word solutions in the output file name.
 #' @export rres_render_solutions_date
 #'
 #' @examples
-#' # Use the following YAML setting after removing the leading comment
-#' # knit: rresutils::rres_render_solutions_date
-
+#' # rresutils::rres_render_solutions_date("my_path/file")
 rres_render_solutions_date <- function(input, fext = "html", ...) {
   quarto::quarto_render(
     input,
     output_file = paste0(
-      xfun::sans_ext(input), '_solutions-', Sys.Date(), '.', fext),
+      xfun::sans_ext(input), "_solutions-", Sys.Date(), ".", fext
+    ),
     execute_params = list(solutions = "false")
-  );
+  )
   quarto::quarto_render(
     input,
     output_file = paste0(
-      xfun::sans_ext(input), '_student-', Sys.Date(), '.', fext),
+      xfun::sans_ext(input), "_student-", Sys.Date(), ".", fext
+    ),
     execute_params = list(solutions = "false")
   )
 }
@@ -36,33 +39,35 @@ rres_render_solutions_date <- function(input, fext = "html", ...) {
 
 #' Render two versions of a file, with and without solutions
 #'
-#' This function assumes the YAML header of an R Markdown file uses a parameter called solutions, params: solutions which has two values, TRUE and FALSE. The file may use this parameter to adjust the r markdown chunk options such as eval or echo when rendering to output the solutions to exercises or not.
+#' This function assumes the YAML header of a .qmd file uses a parameter
+#' called solutions, params: solutions which has two values, TRUE and FALSE.
+#' The file may use this parameter to adjust the R markdown chunk options
+#' such as eval or echo when rendering to output the solutions to exercises.
 #'
-#' The function may be placed in the YAML header of an R Markdown document where it will be invoked on the first output type.
+#' The function works as a standalone function called in the console or script.
 #'
-#' See Rmarkdown Cookbook Section 17.5 as a reference
-#' https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html
 #'
 #' @param input text: the name of the file to be rendered
 #' @param fext text for the file extension
 #' @param ... text: other parameters to be passed to the render function
 #'
-#' @return creates two output files in the default directory. . One has no solutions in the output and the second has solutions and the word solutions in the output file name.
+#' @return creates two output files in the default directory.
+#' The one with no solutions in the output has the word student in the file name.
+#' The one with solutions has the word solutions in the file name.
 #' @export rres_render_solutions
 #'
 #' @examples
-#' # Use the following YAML setting after removing the leading comment
-#' # knit: rresutils::rres_render_solutions
+#' # rresutils::rres_render_solutions("my_path/file")
 #'
 rres_render_solutions <- function(input, fext = "html", ...) {
   quarto::quarto_render(
     input,
-    output_file = paste0(xfun::sans_ext(input), '_solutions.', fext),
+    output_file = paste0(xfun::sans_ext(input), "_solutions.", fext),
     execute_params = list(solutions = "true")
-  );
+  )
   quarto::quarto_render(
     input,
-    output_file = paste0(xfun::sans_ext(input), '_student.', fext),
+    output_file = paste0(xfun::sans_ext(input), "_student.", fext),
     execute_params = list(solutions = "false")
   )
 }
--- README.md
+++ README.md
@@ -12,40 +12,64 @@ The goal of rresutils is to support teaching of R and R Markdown topics.
 
 You can install the released version of rresutils from GitHub with:
 
-devtools::install\_github(“rressler/rresutils”)
+devtools::install_github(“rressler/rresutils”)
 
 ## Examples
 
-The package has two similar functions. These are based on ideas and code
-samples in section 17.5 of the [R Markdown
+The package has two sets of similar functions. These are based on ideas
+and code samples in section 17.5 of the [R Markdown
 Cookbook](https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html)
 
-### knitr utilities
+### `quarto_render()` Functions
+
+#### `rres_render_solutions()`
+
+- This function outputs two versions of an .qmd file that has been
+  configured to use a YAML parameter called `params:solutions` where the
+  values are either `true` or `false`. These values turn code chunk
+  options such as `eval`, `echo`, or `include` on and off respectively.
+
+Use the console to run
+`rresutils::rres_render_solutions("my_path/file.qmd")`.
+
+- The file with solutions will have “\_solutions” in the file name.
+- The file without solutions will have “\_student” in the file name.
+
+The default file extension is “html” but you can change to another
+extension with argument `fext = "pdf"`.
+
+#### `rres_render_solutions_date()`
+
+This function operates the same as `rres_render_solutions()` but it
+appends the `Sys.Date()` value at the end of the file name.
+
+Use the console to run
+`rresutils::rres_knit_solutions_date("my_path/file.qmd")`.
+
+### `rmarkdown::render()` Functions
 
 #### `rres_knit_solutions()`
 
--   This function outputs two versions of an .Rmd file that has been
-    configured to use a YAML parameter called `params:solutions` where
-    the values are either `TRUE` or `FALSE`. These values turn code
-    chunk options such as `eval` or `echo` on and off respectively.
+- This function outputs two versions of an .Rmd file that has been
+  configured to use a YAML parameter called `params:solutions` where the
+  values are either `TRUE` or `FALSE`. These values turn code chunk
+  options such as `eval` or `echo` on and off respectively.
 
 Enter the following into the YAML header and then knit. It will use the
 first output type in your YAML be it PDF or HTML. The file with
-solutions will have the "\_solutions" in the file name.
+solutions will have the “\_solutions” in the file name.
 
       knit: rresutils::rres_knit_solutions
       
 
 #### `rres_knit_solutions_date()`
 
--   This function outputs two versions of an .Rmd file that has been
-    configured to use a YAML parameter called `params:solutions` where
-    the values are either `TRUE` or `FALSE`. These values turn code
-    chunk options such as `eval` or `echo` on and off respectively.
+This function operates the same as `rres_knit_solutions()` but it
+appends the `Sys.Date()` value at the end of the file name.
 
 Enter the following into the YAML header and then knit. It will use the
 first output type in your YAML be it PDF or HTML. The file with the
-solutions will have "\_solutions" in the file name. Both files will have
+solutions will have “\_solutions” in the file name. Both files will have
 the current date added to the file name.
 
       knit: rresutils::rres_knit_solutions_date
